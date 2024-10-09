feb755b90186789363bcf9642bc5c945d1d5884f
Sven Woltmann <sven@happycoders.eu>
2024-10-09 18:43:59 +0200

    Simplify combiner demo

--- src/main/java/eu/happycoders/stream_gatherers/demo5/CombinerDemoComplete.java
+++ src/main/java/eu/happycoders/stream_gatherers/demo5/CombinerDemoComplete.java
@@ -1,54 +1,27 @@
 void main() {
-
   List<String> words = List.of("the", "be", "two", "of", "and", "a", "in", "that");
 
-  List<String> list = words.parallelStream()
-      .gather(best(Comparator.comparing(String::length)))
-      .toList();
+  Optional<String> first = words.parallelStream()
+      .gather(joining())
+      .findFirst();
 
-  System.out.println(list);
+  System.out.println(first);
 }
 
-private static Gatherer<String, AtomicReference<String>, String> best(
-    Comparator<String> comparator) {
-  // Initializer
-  Supplier<AtomicReference<String>> initializer = AtomicReference::new;
+private static Gatherer<String, StringBuilder, String> joining() {
+  Supplier<StringBuilder> initializer = StringBuilder::new;
 
-  // Integrator
-  Gatherer.Integrator<AtomicReference<String>, String, String> integrator =
-      Gatherer.Integrator.ofGreedy((state, element, _) -> {
-        String bestElement = state.get();
-        if (bestElement == null || comparator.compare(element, bestElement) > 0) {
-          state.set(element);
-        }
-        return true;
-      });
+  Gatherer.Integrator<StringBuilder, String, String> integrator =
+      Gatherer.Integrator.ofGreedy(
+          (state, element, _) -> {
+            state.append(element);
+            return true;
+          });
 
-  // Finisher
-  BiConsumer<AtomicReference<String>, Gatherer.Downstream<? super String>> finisher =
-      (state, downstream) -> {
-        String bestElement = state.get();
-        if (bestElement != null) {
-          downstream.push(bestElement);
-        }
-      };
+  BiConsumer<StringBuilder, Gatherer.Downstream<? super String>> finisher =
+      (state, downstream) -> downstream.push(state.toString());
 
-  // Combiner
-  BinaryOperator<AtomicReference<String>> combiner =
-      (state1, state2) -> {
-        String bestElement1 = state1.get();
-        String bestElement2 = state2.get();
-
-        if (bestElement1 == null) {
-          return state2;
-        } else if (bestElement2 == null) {
-          return state1;
-        } else if (comparator.compare(bestElement1, bestElement2) > 0) {
-          return state1;
-        } else {
-          return state2;
-        }
-      };
+  BinaryOperator<StringBuilder> combiner = StringBuilder::append;
 
   return Gatherer.of(initializer, integrator, combiner, finisher);
 }
